53e20061f50b80e6a133ffb4b7f844fea4cc993c
blissy <restricted.karki@gmail.com>
2026-08-27 14:27:18 +0545

    build script now stores lib binaries in build folder

--- core/build-wasm.sh
+++ core/build-wasm.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
+mkdir -p "./build/wasm"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 EMSDK_DIR="${REPO_ROOT}/deps/emsdk"
 WA_SQLITE_DIR="${REPO_ROOT}/wa-sqlite"
-PACKAGE_DIR="${SCRIPT_DIR}/packages/crsqlite-wasm"
 
 export CRSQLITE_COMMIT_SHA="$(git -C "${REPO_ROOT}" rev-parse HEAD)"
 echo "crcommit ${CRSQLITE_COMMIT_SHA}"
 
-mkdir -p "${PACKAGE_DIR}/dist"
 
 pushd "${EMSDK_DIR}" >/dev/null
 ./emsdk install 3.1.45
@@ -22,6 +21,6 @@ popd >/dev/null
 pushd "${WA_SQLITE_DIR}" >/dev/null
 MAKE="${MAKE:-$(command -v gmake || command -v make)}"
 "${MAKE}"
-cp dist/crsqlite.wasm "${PACKAGE_DIR}/dist/crsqlite.wasm"
-cp dist/crsqlite.mjs "${PACKAGE_DIR}/src/crsqlite.mjs"
+cp dist/crsqlite.wasm "../core/build/wasm/crsqlite.wasm"
+cp dist/crsqlite.mjs "../core/build/wasm/crsqlite.mjs"
 popd >/dev/null
